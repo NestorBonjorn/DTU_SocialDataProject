db0d819c366fc74da501e49934b53c64874ce831
NestorBonjorn <nestorbonjorn@gmail.com>
2017-03-30 23:13:19 +0200

    Javascript scripts

--- js/NYCmap.js
+++ js/NYCmap.js
@@ -1,25 +1,25 @@
 //Width and height
-var w = 650;
-var h = 650;
+var wMap = 650;
+var hMap = 650;
+
+//Create SVG element
+var svgMap = d3.select("#mapSection .svg-div")
+			.append("svg")
+			.attr("width", wMap)
+			.attr("height", hMap);
 
 //Define map projection
 var projection = d3.geo.mercator()
 				.center([-73.94, 40.70])
 				.scale(55000)
-				.translate([w/2, h/2]);
+				.translate([wMap/2, hMap/2]);
 
 //Define path generator
 var path = d3.geo.path()
 				 .projection(projection);
 
-//Create SVG element
-var svgMap = d3.select("#mapSection .svg-div")
-			.append("svg")
-			.attr("width", w)
-			.attr("height", h);
-
 //Load in GeoJSON data
-d3.json("Datasets/nyc.geojson", function(json) {
+d3.json("../Datasets/nyc.geojson", function(json) {
 	
 	//Bind data and create one path per GeoJSON feature
 	svgMap.selectAll("path")
